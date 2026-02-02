89aad9feed36f0d3033c599277ae4cd94e2a2b4e
Julian-Win-Stack <9vfxzg7vhz@privaterelay.appleid.com>
2026-02-02 12:46:53 -0800

    Migrate React app from JavaScript to TypeScript

---
+++ App.tsx
@@ -0,0 +1,84 @@
+import { useState, useRef, useEffect } from "react"
+import Die from "./Die"
+import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
+
+type Die = {
+    value:number,
+    isHeld:boolean,
+    id:string
+}
+
+export default function App() {
+    const [dice, setDice] = useState<Die[]>(generateAllNewDice)
+    const buttonRef = useRef<HTMLButtonElement | null>(null)
+
+    const firstDie = dice[0];
+
+    const gameWon = firstDie !== undefined &&
+        dice.every(die => die.isHeld) && 
+        dice.every(die => die.value === firstDie.value)
+        
+    useEffect(() => {
+        const rollDiceBtn = buttonRef.current;
+        if (gameWon && rollDiceBtn) {
+            rollDiceBtn.focus()
+        }
+    }, [gameWon])
+
+    function generateAllNewDice():Die[] {
+        return new Array(10)
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            }))
+    }
+    
+    function rollDice():void {
+        if (!gameWon) {
+            setDice(oldDice => oldDice.map(die =>
+                die.isHeld ?
+                    die :
+                    { ...die, value: Math.ceil(Math.random() * 6) }
+            ))
+        } else {
+            setDice(generateAllNewDice())
+        }
+    }
+
+    function hold(id:string):void {
+        setDice(oldDice => oldDice.map(die =>
+            die.id === id ?
+                { ...die, isHeld: !die.isHeld } :
+                die
+        ))
+    }
+
+    const diceElements = dice.map(dieObj => (
+        <Die
+            key={dieObj.id}
+            value={dieObj.value}
+            isHeld={dieObj.isHeld}
+            hold={() => hold(dieObj.id)}
+        />
+    ))
+
+    return (
+        <main>
+            {gameWon && <Confetti />}
+            <div aria-live="polite" className="sr-only">
+                {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+            </div>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
+                {gameWon ? "New Game" : "Roll"}
+            </button>
+        </main>
+    )
+}
---
+++ Die.tsx
@@ -0,0 +1,22 @@
+
+
+type DieProps = {
+    isHeld:boolean,
+    value:number , 
+    hold: ()=>  void, 
+}
+export default function Die(props:DieProps) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+    
+    return (
+        <button 
+            style={styles}
+            onClick={props.hold}
+            aria-pressed={props.isHeld}
+            aria-label={`Die with value ${props.value}, 
+            ${props.isHeld ? "held" : "not held"}`}
+        >{props.value}</button>
+    )
+}
